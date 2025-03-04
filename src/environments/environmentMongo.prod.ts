096495dd2dba66458afd57534aa1280bce96a7ab
mendezagus <agustin.mendez@pvs.com.ar>
2025-03-04 16:14:11 -0300

    añadi environmentMongo

--- src/environments/environmentMongo.prod.ts
+++ src/environments/environmentMongo.prod.ts
@@ -1,4 +1,4 @@
-export const environmentMongoProd = {
+export const environmentMongo = {
     production: true,
     apiUrl: 'https://coffe-manager-api-1.onrender.com/api'  // URL del backend en Express
   };
