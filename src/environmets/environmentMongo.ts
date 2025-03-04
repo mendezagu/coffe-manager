843575bc684c1cba84c4a5b0f1ab4d77e3deddde
mendezagus <agustin.mendez@pvs.com.ar>
2025-03-04 15:08:31 -0300

    cambio en angular json

---
+++ src/environmets/environmentMongo.ts
@@ -0,0 +1,5 @@
+export const environmentMongo = {
+    production: false,
+    //apiUrl: 'http://localhost:3000/api'  // URL del backend en Express
+    apiUrl: 'https://coffe-manager-api-1.onrender.com/api'  // URL del backend en Express
+  };
---
+++ src/environmets/environmentMongoProd.ts
@@ -0,0 +1,4 @@
+export const environmentMongoProd = {
+    production: true,
+    apiUrl: 'https://coffe-manager-api-1.onrender.com/api'  // URL del backend en Express
+  };
